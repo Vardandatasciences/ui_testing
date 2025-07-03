1cdbaec31374f14c9d99448d0bf648f04a29e57b
syam-916 <munisyam.p@vardaanglobal.com>
2025-07-03 17:27:56 +0530

    demo done module 2

--- test_db.sql
+++
@@ -1,7 +0,0 @@
--- Check that all tables exist
-SHOW TABLES LIKE 'rbac%';
-
--- Verify sample data
-SELECT COUNT(*) FROM users;        -- Should be 18
-SELECT COUNT(*) FROM rbac;         -- Should be 18
-SELECT COUNT(*) FROM rbac_role_permission_matrix; -- Should be 94
